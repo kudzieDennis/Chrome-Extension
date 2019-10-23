b38c171f582d378219f84eac40e04d18524e304e
Fau <48482580+FauTendy@users.noreply.github.com>
2019-10-23 06:26:41 +0200

    Add files via upload
    changes in
    background
    popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,5 @@
-//alert("propaganda noun /ˌprɑp·əˈɡæn·də information or ideas that are spread by an organized group or government to influence people’s opinions, esp. by not giving all the facts or by secretly emphasizing only one way of looking at the facts")
-// popup.js
-//chrome.browseraction.onLoad.addEventListener("load",
-   // function(request, sender, sendResponse) {
-     // if( request.text === "clicked_brower_action" ) {
-       // var firstHref = $("a[href^='popup.html']").eq(0).attr("href");
-  
-        //console.log(window.onload);
-      //}
-   // }
-  //)
-  chrome.window.onload = function(){
-    Text.getElementById("popup.html").onload=function(){
-        alert("propaganda");
+
+  window.onload = function(){
+    var popup = document.getElementById("propagandawarning").innerText;
+        alert("popup");
     }
-}
